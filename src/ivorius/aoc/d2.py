3e54be369491b0b18f0acbb3fa8a2cfbf6eb9207
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-12-02 10:58:01 +0100

    d2 p2

--- src/ivorius/aoc/d2.py
+++ src/ivorius/aoc/d2.py
@@ -12,11 +12,20 @@ def get_score(theirs, mine):
     return (mine + 1) + (compare(theirs, mine) + 1) * 3
 
 
+def get_score_p1(theirs, mine):
+    return get_score(theirs, mine)
+
+
+def get_score_p2(theirs, outcome):
+    mine = (theirs + outcome - 1) % 3
+    return get_score(theirs, mine)
+
+
 total_score = 0
 
 for line in input_str.split("\n"):
-    theirs = ord(line[0]) - ord("A")
-    mine = ord(line[2]) - ord("X")
-    total_score += get_score(theirs, mine)
+    a = ord(line[0]) - ord("A")
+    b = ord(line[2]) - ord("X")
+    total_score += get_score_p2(a, b)
 
 print(total_score)
